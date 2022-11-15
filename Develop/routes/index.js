e66fb2f7ec7f63eaaaeb6dbe2aeb18a8f95dd747
Christian Luna <christianlunaduenas@gmail.com>
2022-11-14 17:19:23 -0800

    Recommiting my the routes and notes.html.

---
+++ Develop/routes/index.js
@@ -0,0 +1,23 @@
+
+const fs = require ('fs');
+const router = require ('express').Router();
+let dbjson = require ('../db.json');
+
+// getting the information from notes and creating it in json form. 
+router.get ('/notes', (req, res) => {
+    dbjson = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+    res.json (dbjson)
+})
+// this is posting your note as well pushing to json file.
+router.post ('/notes', (req, res) => {
+    let noteModel = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random()*1000)
+    }
+    dbjson.push (noteModel)
+    fs.writeFileSync('./db.json', JSON.stringify(dbjson))
+    res.json(dbjson)
+})
+
+module.exports = router
